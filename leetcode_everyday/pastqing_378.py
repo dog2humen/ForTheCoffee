174cc0d78d10e052fa50e4a91d7c5683afe6baf1
pastqing <116676671@qq.com>
2020-07-02 22:52:52 +0800

    add binary search solution

--- leetcode_everyday/pastqing_378.py
+++ leetcode_everyday/pastqing_378.py
@@ -5,7 +5,8 @@ import heapq
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        return self.kthSmallest_v1(matrix, k)
+        #return self.kthSmallest_v1(matrix, k)
+        return self.kthSmallest_v2(matrix, k)
     def kthSmallest_v1(self, matrix: List[List[int]], k: int) -> int:
         '''
             遍历数组, 构建堆, 利用堆找第k小
@@ -32,13 +33,45 @@ class Solution:
 
 
     def kthSmallest_v2(self, matrix: List[List[int]], k: int) -> int:
-        pass
+        '''
+            二分查找:
+            思路: 
+            1. 对值域进行二分, 范围是[matrix[0][0], matrix[-1][-1]], 即左上角和右下角
+            2. 第k小值就在这个范围内
+            3. 取范围的mid值, 找出矩阵中<=mid值的个数c, 如果c < k 则意味着mid值小了, 如果 c > k 则意味着mid值大了
+            4. 如何统计不大于mid值的元素个数:
+                与每行最右边数比, 如果比它大则往走到下一行
+
+
+        '''
+        low, high = matrix[0][0], matrix[-1][-1]
+        while low <= high:
+            mid = low + (high - low) // 2
+            c = self.getMatrixCount(matrix, mid)
+            if c < k:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return low
+    
+    def getMatrixCount(self, matrix, val):
+        c = 0
+        row, col = 0, len(matrix[0]) - 1
+        while row < len(matrix) and col >= 0:
+            if val >= matrix[row][col]:
+                c += col + 1
+                row += 1
+            else:
+                col -= 1
+
+        return c
 
 
 if __name__ == '__main__':
     input = [[1, 5, 9], [10, 11, 13], [12, 13, 15]]
     obj = Solution()
-    res = obj.kthSmallest_v1(input, 2)
+    res = obj.kthSmallest_v2(input, 8)
     print(res)
 
             
