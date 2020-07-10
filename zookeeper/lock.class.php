e22f3c8c0d4a2fe2271fc87c37cdd8f0888ab4bd
chalife <tianxiaofei@itv.baidu.com>
2020-07-10 22:32:03 +0800

    zk

--- zookeeper/lock.class.php
+++ zookeeper/lock.class.php
@@ -8,6 +8,7 @@ class lock
     private static $zk_obj;
 
     private static $global_path='/lock';
+    private static $znode = '';
 
     private function __construct($ip, $port)
     {
@@ -35,26 +36,43 @@ class lock
 
     }
 
+    private static function watcher(){
+        return self::lock()
+    }
 
-    public static function lock()
+    public static function lock(){
+        if(empty(self::$znode)){
+                self::$znode = self::create_tmp_node(self::$global_path);
+            }
+        return self::get_lock()
+    }
+
+
+    public static function get_lock()
     {
-        self::$znode = str_replace($global_path. '/', '', self::$zode); = self::create_tmp_node(self::$global_path);
+
+
         $childrens = self::$zk_obj->getchildren(self::$global_path));
         if(!empty($childrens)
         {
             sort($childrens);
-            if($childrens[0]== self::$znode)
+            if($childrens[0] == self::$znode)
             {
                 reutrn true;
             }
+        }
+        else
+        {
+            $result = self::$zk_obj->get(self::$global_path,self::watcher);
         }
         return false;
 
     }
 
+
     public static function release()
     {
-
+        return self::$zk_obj->delete(self::$znode);
     }
 
 }
