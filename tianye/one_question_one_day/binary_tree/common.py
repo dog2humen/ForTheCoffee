9abdaeea9e59ff6fa8ad08d5dc57fb84c7edfa9c
tianye28 <duoduoty@163.com>
2020-07-13 19:44:23 +0800

    binary tree basic operation

---
+++ tianye/one_question_one_day/binary_tree/common.py
@@ -0,0 +1,18 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class binary_tree_basic_operation(object):
+    def pre_order_cursive(self, root):
+
+    def pre_oder(self, root):
+
+    def in_order_cursive(self, root):
+
+    def in_order(self, root):
+
+    def post_order_cursive(self, root):
+
+    def post_order(self, root):
