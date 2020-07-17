1e26fe2cf8e31a4d998e26062dc88298dc5aa3aa
施宇 <shiyu@codecor.cn>
2020-07-17 15:21:18 +0800

    多练手

---
+++ leetcode_everyday/shiyu_35.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int i;
+    int numsSize = 10;
+    int *nums = (int *)malloc(sizeof(int) * numsSize);
+
+    for(i = 0;i<numsSize;i++) {
+        *(nums+i) = i*2;
+    }
+
+    printf("%d\n", searchInsert(nums, numsSize, 6));
+
+    free(nums);
+
+    return 0;
+}
+
+int searchInsert(int* nums, int numsSize, int target){
+    int half = 0, left = 0, right = numsSize - 1, pos = numsSize;
+
+    while(left <= right) {
+        half = ((right - left) >> 1) + left;
+
+        if (*(nums+half) >= target) {
+            pos = half;
+            right = half - 1;
+        }else{
+            left = half + 1;
+        }
+    }
+
+    return pos;
+}
