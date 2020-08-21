f77ae9f3f9f2314ca7729fef81f640a657d906a4
tianye28 <duoduoty@163.com>
2020-08-21 12:24:03 +0800

    binary tree

--- tianye/one_question_one_day/binary_tree/common.py
+++ tianye/one_question_one_day/binary_tree/common.py
@@ -159,3 +159,72 @@ class Solution(object):
         root.left = self.build_BST(arr, start, mid - 1)
         root.right = self.build_BST(arr, mid + 1, end)
         return root
+
+
+    """
+    111. 二叉树的最小深度
+        给定一个二叉树，找出其最小深度。
+
+        最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+        说明: 叶子节点是指没有子节点的节点。
+
+        示例:
+
+        给定二叉树 [3,9,20,null,null,15,7],
+
+            3
+           / \
+          9  20
+            /  \
+           15   7
+        返回它的最小深度  2.
+    """
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        return self.minDepth_v1(root)
+
+    # dfs
+    def minDepth_v1(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        if not root.left and not root.right:
+            return 1
+
+        l = self.minDepth(root.left)
+        r = self.minDepth(root.right)
+
+        if not root.left or not root.right:
+            return l + r + 1
+
+        return min(l, r) + 1
+
+    # bfs
+    def minDepth_v2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        import collections
+        queue = collections.deque([(root, 1)])
+        while queue:
+            node, depth = queue.popleft()
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                queue.append((node.left, depth + 1))
+            if node.right:
+                queue.append((node.right, depth + 1))
+
+        return 0
